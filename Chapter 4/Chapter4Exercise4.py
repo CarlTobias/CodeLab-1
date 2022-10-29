474a47bbd2537a3c8a12822bc5ecee73f94253e8
Carl Tobias <saibotrics@icloud.com>
2022-10-29 19:07:55 +0400

    edit

--- Chapter 4/Chapter4Exercise4.py
+++ Chapter 4/Chapter4Exercise4.py
@@ -2,46 +2,46 @@ print("\nExercise 4\n")
 
 print("Wlcome to the Life Stage Determiner!\n")
 
-age = input("Enter age: ")
+age = int(input("Enter age: "))
 
 if age < 2:
     print("You are a baby. GooGooGahGah.")
 
-elif age >= 2:
+elif age < 4:
     print("You are a toddler. Wah Wah.")
 
-elif age <= 4:
+elif age < 13:
     print("You are a kid. Enjoy every moment. Adulthood sucks.")
 
-elif age <= 13:
+elif age < 20:
     print("You are a teen. Stop making everyone so miserable.")
 
-elif age <= 20:
+elif age < 65:
     print("Ew. You are an adult. Go get a job.")
 
-elif age >= 65:
+elif age > 65:
     print("You are an elder. Gimme some wisdom Gramps/Grams!")
 
-else:
-    age = ("Please enter a number: ")
+elif age <= -1:
+    age = int(input("Please enter a valid age: "))
 
     if age < 2:
         print("You are a baby. GooGooGahGah.")
     
-    elif age >= 2:
+    elif age < 4:
         print("You are a toddler. Wah Wah.")
     
-    elif age <= 4:
+    elif age < 13:
         print("You are a kid. Enjoy every moment. Adulthood sucks.")
     
-    elif age <= 13:
+    elif age < 20:
         print("You are a teen. Stop making everyone so miserable.")
     
-    elif age <= 20:
+    elif age < 65:
         print("Ew. You are an adult. Go get a job.")
     
-    elif age >= 65:
+    elif age > 65:
         print("You are an elder. Gimme some wisdom Gramps/Grams!")
     
-    else:
+    elif age <= -1:
         print("Aww, you broke it. Please reset the program.")
