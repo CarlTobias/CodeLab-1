5c043fb4df694117829f38e24b80fe9336bb55f7
Carl Tobias <saibotrics@icloud.com>
2022-10-30 22:44:36 +0400

    finaledit

---
+++ Chapter 4/Chapter4Exercise5.py
@@ -0,0 +1,31 @@
+print("\nExercise 5\n")
+
+favourtie_fruits = ["Sweet Melon", "Strawberry", "Grapes"]
+
+if "Sweet Melon" in favourtie_fruits or "Sweet Melons" in favourtie_fruits:
+    print("Wow! You like sweet melons too? They're my favourite fruit!\n")
+
+elif "Strawberry" in favourtie_fruits or "Strawberries" in favourtie_fruits:
+    print("Wow! You like strawberries too? They're my favourite fruit!\n")
+
+elif "Grape" in favourtie_fruits or "Grapes" in favourtie_fruits:
+    print("Wow! You like sweet melons too? They're my favourite fruit!\n")
+
+else:
+    print("You don't like any fruits?? You have a problem.\n")
+
+favourtie_fruits.pop()
+favourtie_fruits.pop(0)
+favourtie_fruits.pop(1)
+
+if "Sweet Melon" in favourtie_fruits or "Sweet Melons" in favourtie_fruits:
+    print("Wow! You like sweet melons too? They're my favourite fruit!\n")
+
+elif "Strawberry" in favourtie_fruits or "Strawberries" in favourtie_fruits:
+    print("Wow! You like strawberries too? They're my favourite fruit!\n")
+
+elif "Grape" in favourtie_fruits or "Grapes" in favourtie_fruits:
+    print("Wow! You like sweet melons too? They're my favourite fruit!\n")
+
+else:
+    print("You don't like any fruits?? You have a problem.\n")
