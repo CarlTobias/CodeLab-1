8960240a74145a595035dab37c3702f1a98728a0
Carl Tobias <saibotrics@icloud.com>
2022-10-30 15:59:57 +0400

    chap5woot

--- Chapter 5/Chapter5Exercise1.py
+++ Chapter 5/Chapter5Exercise1.py
@@ -1,1 +1,50 @@
 print("\nExercise 1\n")
+
+personal_info = {"First Name": input("First Name: "),
+                "Last Name": input("Last Name: "),
+                "Age": input("Age: "),
+                "City": input("City: "),
+                "Country": input("Country: ")}
+
+print("First Name: " + personal_info["First Name"])
+print("Last Name: " + personal_info["Last Name"])
+print("Age: " + personal_info["Age"])
+print("City: " + personal_info["City"])
+
+yno = input("\nIs this information correct? ").upper()
+
+if yno == "YES":
+    print("Data is saved. Thank you for your cooperation.")
+
+elif yno == "NO":
+    print("Please try again.\n")
+
+    personal_info = {"First Name": input("First Name: "),
+                     "Last Name": input("Last Name: "),
+                     "Age": input("Age: "),
+                     "City": input("City: "),
+                     "Country": input("Country: ")}
+
+    print("First Name: " + personal_info["First Name"])
+    print("Last Name: " + personal_info["Last Name"])
+    print("Age: " + personal_info["Age"])
+    print("City: " + personal_info["City"])
+
+    yno = input("\nIs this information correct? ").upper()
+
+    if yno == "YES":
+        print("Data is saved. Thank you for your cooperation.")
+    
+    elif yno == "NO":
+        print("Please try again.\n")
+        
+        personal_info = {"First Name": input("First Name: "),
+                         "Last Name": input("Last Name: "),
+                        "Age": input("Age: "),
+                        "City": input("City: "),
+                        "Country": input("Country: ")}
+    else:
+        print("Error. Reset the program.")
+
+else:
+    print("Error. Reset the program.")
