a5ae09d669cd510ed5b163f1b9432b921fccbc96
Carl Tobias <saibotrics@icloud.com>
2022-10-30 23:13:50 +0400

    chap5ex2

--- Chapter 5/Chapter5Exercise2.py
+++ Chapter 5/Chapter5Exercise2.py
@@ -1,3 +1,94 @@
 print("\nExercise 2\n")
 
+print("Welcome to the Metaverse Creative Computing Glossary!\n")
 
+glossary = {
+    "print": "The print() function prints the specified message to the screen, or other standard output device.",
+    "int": "The int() function converts the specified value into an integer number.",
+    "if-else": "The if-else statement is used to execute both the true part and false part of a given condition."
+}
+
+choice = input("What word do you want defined today? ").upper()
+
+if choice == "PRINT":
+    print("print():\n  - " + glossary["print"])
+    print("\nThank you for using our glossary! See you soon.")
+    feedback = input("\n\nPlease tell us what more we could do to enhance your experience.\nFeedback: ")
+    print("\nThank you!")
+
+elif choice == "INT":
+    print("print():\n  - " + glossary["int"])
+    print("\nThank you for using our glossary! See you soon.")
+    feedback = input("\n\nPlease tell us what more we could do to enhance your experience.\nFeedback: ")
+    print("\nThank you!")
+
+elif choice == "IF-ELSE" or choice == "IF ELSE" or choice == "IFELSE":
+    print("print():\n  - " + glossary["if-else"])
+    print("\nThank you for using our glossary! See you soon.")
+    feedback = input("\n\nPlease tell us what more we could do to enhance your experience.\nFeedback: ")
+    print("\nThank you!")
+
+else:
+    print("Oh, I'm sorry. Our glossary is still being updated. Try that word again soon.\n")
+
+    choice2 = input("Would you like to define another word? ").upper()
+
+    if choice2 == "YES":
+        choice = input("\nWhat word do you want defined? ").upper()
+        
+        if choice == "PRINT":
+            print("print():\n  - " + glossary["print"])
+            print("\nThank you for using our glossary! See you soon.")
+            feedback = input("\n\nPlease tell us what more we could do to enhance your experience.\nFeedback: ")
+            print("\nThank you!")
+        
+        elif choice == "INT":
+            print("print():\n  - " + glossary["int"])
+            print("\nThank you for using our glossary! See you soon.")
+            feedback = input("\n\nPlease tell us what more we could do to enhance your experience.\nFeedback: ")
+            print("\nThank you!")
+        
+        elif choice == "IF-ELSE" or choice == "IF ELSE" or choice == "IFELSE":
+            print("print():\n  - " + glossary["if-else"])
+            print("\nThank you for using our glossary! See you soon.")
+            feedback = input("\n\nPlease tell us what more we could do to enhance your experience.\nFeedback: ")
+            print("\nThank you!")
+        
+        else:
+            print("\nI'm sorry, we still haven't added that word yet as well. Please try again soon!")
+    
+    elif choice2 == "NO":
+        print("\nAlright. Thank you! See you soon.")
+
+    else:
+        choice2 = input("\nYes or No? ").upper()
+
+        if choice2 == "YES":
+            choice = input("\nWhat word do you want defined? ").upper()
+            
+            if choice == "PRINT":
+                print("print():\n  - " + glossary["print"])
+                print("\n\nThank you for using our glossary! See you soon.")
+                feedback = input("\n\nPlease tell us what more we could do to enhance your experience.\nFeedback: ")
+                print("\nThank you!")
+
+            elif choice == "INT":
+                print("print():\n  - " + glossary["int"])
+                print("\nThank you for using our glossary! See you soon.")
+                feedback = input("\n\nPlease tell us what more we could do to enhance your experience.\nFeedback: ")
+                print("\nThank you!")
+
+            elif choice == "IF-ELSE" or choice == "IF ELSE" or choice == "IFELSE":
+                print("print():\n  - " + glossary["if-else"])
+                print("\nThank you for using our glossary! See you soon.")
+                feedback = input("\n\nPlease tell us what more we could do to enhance your experience.\nFeedback: ")
+                print("\nThank you!")
+
+            else:
+                print("\nI'm sorry, we still haven't added that word yet as well. Please try again soon!")
+        
+        elif choice2 == "NO":
+            print("\nAlright. Thank you! See you soon.")
+        
+        else:
+            print("\nUh oh, It looks like you made a booboo. Please reset the program and try again.\n")
