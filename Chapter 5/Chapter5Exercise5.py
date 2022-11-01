4264ff41cdb769a785e465238ee25c9571d8fc62
Carl Tobias <saibotrics@icloud.com>
2022-11-01 14:06:44 +0400

    sss

--- Chapter 5/Chapter5Exercise5.py
+++ Chapter 5/Chapter5Exercise5.py
@@ -1,2 +1,29 @@
 print("\nExercise 5\n")
 
+canines = {
+    "Owner's Name" : "Amihan Anderson",
+    "Breed" : "Siberian Husky",
+    "Pet Name" : "Bark Obama",
+    "Pet Age" : "4"
+}
+
+felines = {
+    "Owner's Name": "Samantha Renee",
+    "Breed": "Ragdoll",
+    "Pet Name": "Doja",
+    "Pet Age": "2"
+}
+
+reptiles = {
+    "Owner's Name": "Kyle Reeferson",
+    "Breed": "Ball Python",
+    "Pet Name": "Ekans",
+    "Pet Age": "6"
+}
+
+pets = [canines, felines, reptiles]
+
+for animals in pets:
+    print("\nDetails:\n")
+    for infoneeded, infogiven in animals.items():
+        print(infoneeded + ": " + infogiven)
